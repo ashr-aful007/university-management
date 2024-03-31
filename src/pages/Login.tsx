c03bdd15c760e91074a454788c508fa70a9ffa97
ashr-aful007 <ashraful99980@gmail.com>
2024-03-31 22:40:15 +0600

    successfully get refresh token

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,30 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form"
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 
 function Login() {
+        const {register, handleSubmit} = useForm()
+   const [login, {data, error}] = useLoginMutation()
+        const onSubmit = (data) =>{
+            const userInfo ={
+               id: data.userID,
+               password: data.password,
+            }
+            login(userInfo)
+        }
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register('userID')}/>
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" {...register('password')}/>
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   )
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './features/auth/authSlice'
+import { baseApi } from './api/baseApi'
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,  //conect base api
     auth: authReducer,
   },
+  middleware: getDefaultMiddlewares => getDefaultMiddlewares().concat(baseApi.middleware) //conect base api
+      
+  
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
