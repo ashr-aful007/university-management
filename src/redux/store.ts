45306bcc7b1ded8395877871c915383a0d30e04e
ashr-aful007 <ashraful99980@gmail.com>
2024-04-01 01:43:02 +0600

    add redux persist for refresh token localStorage handle

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './features/auth/authSlice'
 import { baseApi } from './api/baseApi'
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+//using persist for localStorage token 
+const persistConfig ={
+    key: 'auth',
+    storage,
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,  //conect base api
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
-  middleware: getDefaultMiddlewares => getDefaultMiddlewares().concat(baseApi.middleware) //conect base api
+  middleware: getDefaultMiddlewares => getDefaultMiddlewares({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],  //ignore serializable check for persist
+    },
+  }).concat(baseApi.middleware) //conect base api
       
   
 })
@@ -16,3 +38,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+//persistor for localStore token set
+export const persistor = persistStore(store)
